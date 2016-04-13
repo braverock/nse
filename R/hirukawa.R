45d0e15216832abe3afdabf523d28796eb81e0aa
David Ardia <david.ardia.ch@gmail.com>
2016-04-13 14:23:02 +0000

    *** empty log message ***
    git-svn-id: svn://r-forge.r-project.org/svnroot/nse/pkg/nse@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/hirukawa.R
+++ R/hirukawa.R
@@ -1,59 +1,56 @@
 # Automatic Bandwidth selection for Bartlett and Parzen Kernel
 # Describe in A Two-Stage Plug-In Bandwidth Selection And Its Implementation For Covariance Estimation
 
-.f.hiruk.bandwidth.solve <- function(y, kernel, prewhite){
-
-  bandwidth.solve = function(y, alpha ,q, kq, intk2, intx2k2,st0, kernel) {
+.f.hiruk.bandwidth.solve <- function(y, type = c("bartlett", "parzen"), lag.prewhite = 0){
+  
+  bandwidth.solve = function(y, alpha, q, kq, intk2, intx2k2, st0, f.kernel) {
     n   = length(y)
     bt  = ((alpha^2)*intk2*st0^(2*q+1)/((2*q+1)*intx2k2))^(1/(4*q+1)) # bandwidth
-    rn  = kernel(y,bt,q) # estimate of spectral density at origin
-    rd  = kernel(y,bt,0) # estimate of q'th generalized derivative of spectral density at origin
+    rn  = f.kernel(y, bt, q) # estimate of spectral density at origin
+    rd  = f.kernel(y, bt, 0) # estimate of q'th generalized derivative of spectral density at origin
     r2  = (rn/rd)^2 # squared normalized curvature
     st1 = (q*(kq^2)*r2*n/intk2)^(1/(2*q+1)) # calculate new bandwidth
-
-    #if st1 = st0 optimal bandwidth is found
+    
+    # if st1 = st0 optimal bandwidth is found
     out = abs(st0 - st1)
     return(out)
   }
-
+  
   n = length(y)
-
-  if (isTRUE(prewhite)) {
-  # prewithening of time serie
-    y = ar(y, aic = FALSE, order.max = 1)$resid[2: n]
-  }
-
-  #ar coefficient to cacluclate Alpha(q)
-  arCoefficient = ar(y, aic = FALSE, order.max = 1)$ar[1]
-
-  if (kernel == "Bartlett") {
+  y = f.prewhite(y, ar.order = lag.prewhite)$ar.resid
+  
+  # ar coefficient to cacluclate Alpha(q)
+  arCoefficient = stats::ar(y, aic = FALSE, order.max = 1)$ar[1]
+  
+  type = type[1]
+  if (type == "bartlett") {
     q       = 1 #characteristic exponent of Bartlett kernel
     k1      = 1 #generalized derivative of Bartlett kernel at origin
     intk2   = 2/3 #squared integral of Bartlett kernel
     intx2k2 = 1/15 #2th-order moment of Bartlett kernel
     kernel  = f.bartlett
-  } else if (kernel == "Parzen") {
+  } else if (type == "parzen") {
     q       = 2 #characteristic exponent of Parzen kernel
     k1      = 6 #generalized derivative of Parzen kernel at origin
     intk2   = 151/280 #squared integral of Parzen kernel
     intx2k2 = 929/295680 #4th-order moment of Parzen kernel
     kernel  = f.parzen
-
+    
   } else {
-    stop("only Bartlett, Parzen are supported")
+    stop("Invalid type : must be one of c('bartlett','parzen')")
   }
-
-  #proxy for Alpha(q)
+  
+  # proxy for alpha(q)
   if (q == 1) {
     alpha = (arCoefficient ^ 2  + 1) / (arCoefficient ^ 2 - 1)
-
+    
   } else if (q == 2) {
     alpha = -(arCoefficient ^ 2 + 8 * arCoefficient + 1) / (arCoefficient - 1) ^ 2
   }
-
-  #Function to optimize
+  
+  # function to optimize
   FUN = function(x) bandwidth.solve(y, alpha ,q, k1, intk2, intx2k2, x, kernel)
-  #optimization
+  # optimization
   optimalBandwidth = optimize(f = FUN, lower = 4, upper =  n)
   out = optimalBandwidth$minimum
   return(out)
---
+++ R/prewhitening.R
@@ -0,0 +1,35 @@
+
+#' Function to perform AR prewithening
+f.prewhite = function(x, ar.order = 1) {
+  
+  if (!is.null(ar.order)) {
+    if (ar.order == 0){
+      out = list(ar.resid = x, ar.param = NULL, ar.order = 0, scale = 1)
+      return(out)
+    } 
+  }
+  
+  aic = FALSE
+  if (is.null(ar.order)){
+    aic = TRUE
+    ar.order = min(length(x), 10)
+  }
+  ar.fit = stats::ar.ols(x = x, aic = aic, order.max = ar.order, demean = FALSE)
+  
+  if (inherits(ar.fit, "try-error")) {
+    stop("AR prewhitening of estimating functions failed")
+  } 
+    
+  ar.resid = as.vector(na.omit(ar.fit$resid))
+  ar.param = as.vector(ar.fit$ar)
+  ar.order = as.numeric(ar.fit$order)
+  
+  scale = 1 / (1 - sum(ar.param))^2
+  if (length(scale) == 0) {
+    scale = 1
+  }
+  
+  out = list(ar.resid = ar.resid, ar.param = ar.param, ar.order = ar.order, scale = scale)
+  return(out)
+  #n = n - prewhite
+}
